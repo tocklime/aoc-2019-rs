c98011c4a40f3180bec0111bebf9d02064fd5db1
Greg Manning <1396183+tocklime@users.noreply.github.com>
2019-12-01 10:38:50 +0000

    some d1 tidying up and optimising

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,8 @@
-use itertools::unfold;
+use itertools::{iterate, unfold};
+
+pub fn rocket_fn(x: &i32) -> i32 {
+    (*x / 3) - 2
+}
 
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
@@ -7,40 +11,46 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 
 #[aoc(day1, part1)]
 pub fn part1(input: &[i32]) -> i32 {
-    input.iter().cloned().map(part1_calc).sum()
+    input.iter().map(rocket_fn).sum()
 }
 
-#[aoc(day1, part2)]
+#[aoc(day1, part2, unfold)]
 pub fn part2(input: &[i32]) -> i32 {
-    input.iter().cloned().map(part2_calc).sum()
+    input
+        .iter()
+        .flat_map(|x| {
+            unfold(*x, |last_mass| match rocket_fn(last_mass) {
+                a if a <= 0 => None,
+                a => {
+                    *last_mass = a;
+                    Some(a)
+                }
+            })
+        })
+        .sum()
 }
-
-pub fn part1_calc(x: i32) -> i32 {
-    (x / 3) - 2
-}
-
-pub fn part2_calc(x: i32) -> i32 {
-    unfold(x, |last_mass| match part1_calc(*last_mass) {
-        a if a <= 0 => None,
-        a => {
-            *last_mass = a;
-            Some(a)
-        }
-    })
-    .sum()
+#[aoc(day1, part2, iterate)]
+pub fn part2_2(input: &[i32]) -> i32 {
+    input
+        .iter()
+        .flat_map(|&x| iterate(x, rocket_fn).skip(1).take_while(|&x| x > 0))
+        .sum()
 }
 
 #[test]
 pub fn part1_examples() {
-    assert_eq!(part1_calc(12), 2);
-    assert_eq!(part1_calc(14), 2);
-    assert_eq!(part1_calc(1969), 654);
-    assert_eq!(part1_calc(100756), 33583);
+    assert_eq!(part1(&[12]), 2);
+    assert_eq!(part1(&[14]), 2);
+    assert_eq!(part1(&[1969]), 654);
+    assert_eq!(part1(&[100756]), 33583);
 }
 
 #[test]
 pub fn part2_examples() {
-    assert_eq!(part2_calc(14), 2);
-    assert_eq!(part2_calc(1969), 966);
-    assert_eq!(part2_calc(100756), 50346);
+    assert_eq!(part2(&[14]), 2);
+    assert_eq!(part2(&[1969]), 966);
+    assert_eq!(part2(&[100756]), 50346);
+    assert_eq!(part2_2(&[14]), 2);
+    assert_eq!(part2_2(&[1969]), 966);
+    assert_eq!(part2_2(&[100756]), 50346);
 }
