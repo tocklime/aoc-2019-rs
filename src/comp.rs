e21ddbe5da5e7de532065b120a2d9e08f49ede0c
Greg Manning <1396183+tocklime@users.noreply.github.com>
2019-12-02 19:16:38 +0000

    refactoring

--- src/comp.rs
+++ src/comp.rs
@@ -1,14 +1,45 @@
-#[derive(Clone)]
+use num_enum::TryFromPrimitive;
+use std::convert::TryFrom;
+
+#[derive(Clone, Copy, Debug)]
 pub enum ComputerState {
     RUNNING,
     HALTED,
 }
 
+#[derive(Clone, Copy, TryFromPrimitive)]
+#[repr(usize)]
+pub enum OpCode {
+    Add = 1,
+    Mult = 2,
+    Halt = 99,
+}
+
+impl OpCode {
+    fn arg_count(self) -> usize {
+        match self {
+            OpCode::Add | OpCode::Mult => 3,
+            OpCode::Halt => 0,
+        }
+    }
+    fn execute(&self, c: &mut Computer) {
+        let arg_count = self.arg_count();
+        let args: Vec<usize> = (0..arg_count).map(|x| c.load(x + 1)).collect();
+        match self {
+            OpCode::Add => c.abs_store(args[2], c.abs_load(args[0]) + c.abs_load(args[1])),
+            OpCode::Mult => c.abs_store(args[2], c.abs_load(args[0]) * c.abs_load(args[1])),
+            OpCode::Halt => c.state = ComputerState::HALTED,
+        }
+        c.inc_ip(1 + arg_count)
+    }
+}
+
+#[derive(Debug)]
 pub struct Computer<'a> {
     initial_mem: &'a [usize],
     memory: Vec<usize>,
-    pub instruction_pointer: usize,
-    pub state: ComputerState,
+    instruction_pointer: usize,
+    state: ComputerState,
 }
 
 impl Computer<'_> {
@@ -28,8 +59,15 @@ impl Computer<'_> {
         self.state = ComputerState::RUNNING;
         return self;
     }
-    pub fn current_op(&self) -> usize {
-        self.memory[self.instruction_pointer]
+    pub fn current_op(&self) -> OpCode {
+        let as_int = self.memory[self.instruction_pointer];
+        match OpCode::try_from(as_int) {
+            Ok(x) => return x,
+            Err(_) => panic!(
+                "Unknown op code @ {}: {}\n\n{:?}",
+                self.instruction_pointer, as_int, self
+            ),
+        }
     }
     pub fn abs_load(&self, pos: usize) -> usize {
         self.memory[pos]
@@ -48,38 +86,14 @@ impl Computer<'_> {
     }
     pub fn run(&mut self) -> &Self {
         loop {
-            match self.step() {
+            match self.step().state {
                 ComputerState::HALTED => return self,
                 ComputerState::RUNNING => (),
             }
         }
     }
-    pub fn step(&mut self) -> &ComputerState {
-        match self.current_op() {
-            // ADD
-            1 => {
-                self.abs_store(
-                    self.load(3),
-                    self.abs_load(self.load(1)) + self.abs_load(self.load(2)),
-                );
-                self.inc_ip(4)
-            }
-            // MULT
-            2 => {
-                self.abs_store(
-                    self.load(3),
-                    self.abs_load(self.load(1)) * self.abs_load(self.load(2)),
-                );
-                self.inc_ip(4);
-            }
-            99 => self.state = ComputerState::HALTED,
-            // PANIC
-            _ => panic!(
-                "Unknown OP Code {} at IP {}",
-                self.current_op(),
-                self.instruction_pointer
-            ),
-        }
-        return &self.state;
+    pub fn step(&mut self) -> &Self {
+        self.current_op().execute(self);
+        return self;
     }
 }
