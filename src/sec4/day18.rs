cb4d4f623c8e348d4f6d3039b67dc62ea8a4b458
Greg Manning <1396183+tocklime@users.noreply.github.com>
2019-12-18 13:58:57 +0000

    faster

--- src/sec4/day18.rs
+++ src/sec4/day18.rs
@@ -2,6 +2,39 @@ use crate::utils::points::as_point_map;
 use crate::utils::prelude::*;
 use std::cmp::min;
 
+pub fn search2(
+    map: &HashMap<Point, char>,
+    start: Point,
+) -> HashMap<Point, (char, usize, BTreeSet<char>)> {
+    let mut points = std::collections::VecDeque::new();
+    points.push_back((start, 0, BTreeSet::new()));
+    let mut min_dist_map: HashMap<Point, (char, usize, BTreeSet<char>)> = HashMap::new();
+    let mut been: BTreeSet<Point> = BTreeSet::new();
+    while !points.is_empty() {
+        let (pos, count, keys) = points.pop_front().unwrap();
+        been.insert(pos);
+        pos.neighbours()
+            .iter()
+            .filter(|&p| !been.contains(p))
+            .for_each(|&p2| {
+                if let Some(here) = map.get(&p2) {
+                    if here != &'#' && (here == &'.' || !min_dist_map.contains_key(&p2))
+                    //&& (!here.is_uppercase() || keys.contains(&here.to_ascii_lowercase()))
+                    {
+                        let mut new_keys = keys.clone();
+                        if here.is_lowercase() {
+                            min_dist_map.insert(p2, (*here, count + 1, new_keys.clone()));
+                        }
+                        if here.is_alphabetic() {
+                            new_keys.insert(*here);
+                        }
+                        points.push_back((p2, count + 1, new_keys));
+                    }
+                }
+            });
+    }
+    min_dist_map
+}
 pub fn search(
     map: &HashMap<Point, char>,
     start: Point,
@@ -10,7 +43,7 @@ pub fn search(
     let mut points = std::collections::VecDeque::new();
     points.push_back((start, 0));
     let mut min_dist_map = HashMap::new();
-    let mut been: HashSet<Point> = HashSet::new();
+    let mut been: BTreeSet<Point> = BTreeSet::new();
     while !points.is_empty() {
         let (pos, count) = points.pop_front().unwrap();
         been.insert(pos);
@@ -42,13 +75,13 @@ use std::collections::BTreeSet;
 #[aoc(day18, part1)]
 pub fn p1(input: &str) -> usize {
     let map = as_point_map(input);
-    let at_sym = *map.iter().find(|(_,&v)| v == '@').expect("No @").0;
-    solve(&map,&[at_sym])
+    let at_sym = *map.iter().find(|(_, &v)| v == '@').expect("No @").0;
+    solve(&map, &[at_sym])
 }
 #[aoc(day18, part2)]
 pub fn p2(input: &str) -> usize {
     let mut map = as_point_map(input);
-    let at_sym = *map.iter().find(|(_,&v)| v == '@').expect("No @").0;
+    let at_sym = *map.iter().find(|(_, &v)| v == '@').expect("No @").0;
 
     map.insert(at_sym, '#');
     map.insert(at_sym.up(), '#');
@@ -59,23 +92,42 @@ pub fn p2(input: &str) -> usize {
         at_sym.up().left(),
         at_sym.up().right(),
         at_sym.down().left(),
-        at_sym.down().right()
+        at_sym.down().right(),
     ];
-    solve(&map,&points)
+    solve(&map, &points)
 }
-pub fn solve(map: &HashMap<Point,char>, starts: &[Point]) -> usize {
+pub fn solve(map: &HashMap<Point, char>, starts: &[Point]) -> usize {
     let mut known_bests: HashMap<(Vec<Point>, BTreeSet<char>), usize> = HashMap::new();
     known_bests.insert((starts.to_vec(), BTreeSet::new()), 0);
+    let locations: Vec<Point> = map
+        .iter()
+        .filter_map(|(&k, &v)| if v.is_lowercase() { Some(k) } else { None })
+        .chain(starts.iter().cloned())
+        .collect();
+    let info: HashMap<Point, HashMap<Point, (char, usize, BTreeSet<char>)>> = locations
+        .iter()
+        .map(|&ap| {
+            let reachable = search2(&map, ap);
+            (ap, reachable)
+        })
+        .collect();
+
+    //println!("INFO: {:?}", info);
     loop {
-        let mut new_known_bests : HashMap<(Vec<Point>, BTreeSet<char>), usize> = HashMap::new();
+        let mut new_known_bests: HashMap<(Vec<Point>, BTreeSet<char>), usize> = HashMap::new();
         for ((poss, keys), v) in known_bests.iter() {
-            for (ix,bot) in poss.iter().enumerate(){
-                let available_keys = search(&map, *bot, &keys);
-                for (new_k, (d, p)) in available_keys.iter() {
+            for (ix, bot) in poss.iter().enumerate() {
+                let available_keys = info[bot].iter().filter(|(&a, (c, _, hs))| {
+                    !keys.contains(c) && hs.iter().all(|&i| keys.contains(&i.to_ascii_lowercase()))
+                });
+                //println!("Options: {:#?}", available_keys);
+
+                //let available_keys = search(&map, *bot, &keys);
+                for (new_p, (c, d, _req_keys)) in available_keys {
                     let mut new_keys = keys.clone();
-                    new_keys.insert(*new_k);
+                    new_keys.insert(*c);
                     let mut new_ps = poss.clone();
-                    new_ps[ix] = *p;
+                    new_ps[ix] = *new_p;
                     new_known_bests
                         .entry((new_ps, new_keys))
                         .and_modify(|e| *e = min(*e, d + v))
@@ -86,14 +138,15 @@ pub fn solve(map: &HashMap<Point,char>, starts: &[Point]) -> usize {
         if new_known_bests.is_empty() {
             break;
         }
+        //println!("{:?}", new_known_bests);
         known_bests = new_known_bests;
     }
-    return *known_bests.values().min().expect("No answers?");
+    *known_bests.values().min().expect("No answers?")
 }
 
 #[cfg(test)]
 mod test {
-
+    //                  012345678901234567890123
     const MAZ0: &str = "########################\n\
                         #f.D.E.e.C.b.A.@.a.B.c.#\n\
                         ######################.#\n\
